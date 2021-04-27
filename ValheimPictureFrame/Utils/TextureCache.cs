ab097d1c4971c00f3fb2d1f8db61e2f632c27905
D_first <dntk.daisei@gmail.com>
2021-04-27 22:39:22 +0900

    Release 1.0.0

---
+++ ValheimPictureFrame/Utils/TextureCache.cs
@@ -0,0 +1,52 @@
+﻿using Jotunn.Utils;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace ValheimPictureFrame.Utils
+{
+    public class TextureCache
+    {
+        public string ImageBasePath { get; }
+        private readonly Dictionary<string, Texture> cache = new Dictionary<string, Texture>();
+
+        public TextureCache(string imageBasePath)
+        {
+            ImageBasePath = imageBasePath;
+        }
+
+
+        public Texture Load(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return null;
+            }
+            
+            string textureName = Path.ChangeExtension(name, Path.GetExtension(name).ToLower());
+
+            if (cache.ContainsKey(textureName))
+            {
+                return cache[textureName];
+            }
+
+            
+            if (!(textureName.EndsWith(".jpg") || textureName.EndsWith(".png")))
+            {
+                return null;
+            }
+
+            var texture = AssetUtils.LoadTexture(Path.Combine(ImageBasePath, textureName));
+
+            if (texture == null)
+            {
+                return null;
+            }
+
+            texture.name = textureName;
+            cache.Add(textureName, texture);
+
+            return texture;
+        }
+    }
+}
---
+++ ValheimPictureFrame/ValheimPictureFrame.cs
@@ -0,0 +1,62 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Jotunn;
+using Jotunn.Configs;
+using Jotunn.Entities;
+using Jotunn.Managers;
+using UnityEngine;
+using ValheimPictureFrame.Utils;
+
+namespace ValheimPictureFrame
+{
+    [BepInPlugin("dfirst.ValheimPictureFrame", "Valheim Picture Frame", "1.0.0")]
+    [BepInDependency(Main.ModGuid)]
+    public class ValheimPictureFrame : BaseUnityPlugin
+    {
+        public static readonly TextureCache spriteCache = new TextureCache("ValheimPictureFrame/Assets/Images");
+        public static readonly string TOKEN_NAME = "$piece_dfirst_pictureframe";
+        private static readonly string TOKEN_DESC = "$piece_dfirst_pictureframe_description";
+        private readonly Harmony harmony = new Harmony("dfirst.ValheimPictureFrame");
+
+        private void Awake()
+        {
+            AddPiece();
+            harmony.PatchAll();
+        }
+
+        private void AddPiece()
+        {
+            var assetBundle = AssetBundleHelper.GetAssetBundleFromResources("pictureframe");
+            var asset = assetBundle.LoadAsset<GameObject>($"Assets/Pieces/PictureFrame/PictureFrame.prefab");
+
+            Sign sign = asset.GetComponent<Sign>();
+            sign.m_name = TOKEN_NAME;
+
+            CustomPiece piece = new CustomPiece(asset,
+                new PieceConfig()
+                {
+                    PieceTable = "Hammer",
+                    Description = TOKEN_DESC,
+                    Requirements = new[]
+                    {
+                        new RequirementConfig()
+                        {
+                            Item = "FineWood",
+                            Amount = 6,
+                            Recover = true
+                        },
+                        new RequirementConfig()
+                        {
+                            Item = "BronzeNails",
+                            Amount = 2,
+                            Recover = true
+                        }
+                    }
+                });
+
+            piece.FixReference = true;
+            PieceManager.Instance.AddPiece(piece);
+            assetBundle.Unload(false);
+        }
+    }
+}
