f7e20182ff8c2aaa0d23e91276d26efc382d45c7
D_first <dntk.daisei@gmail.com>
2021-05-15 17:04:12 +0900

    update 1.1.0
    add vertical/square frame
    add change scale system

--- ValheimPictureFrame/Patches/SignTextPatch.cs
+++ ValheimPictureFrame/Patches/SignTextPatch.cs
@@ -1,4 +1,7 @@
 ﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,35 +12,165 @@ namespace ValheimPictureFrame.Patches
         [HarmonyPatch(typeof(Sign), nameof(Sign.SetText))]
         private class SignSetTextPatch
         {
-            static void Postfix(Sign __instance, string ___m_name, Text ___m_textWidget)
+            static void Prefix(string ___m_name, Text ___m_textWidget, ref string __state)
             {
-                UpdatePicture(__instance, ___m_name, ___m_textWidget);
+                if (___m_name == ValheimPictureFrame.TOKEN_NAME)
+                {
+                    __state = ___m_textWidget.text;
+                }
+            }
+
+            static void Postfix(Sign __instance, string ___m_name, Text ___m_textWidget, string __state)
+            {
+                UpdatePicture(__instance, ___m_name, ___m_textWidget, __state);
             }
         }
 
         [HarmonyPatch(typeof(Sign), "UpdateText")]
         private class SignUpdateTextPatch
         {
-            static void Postfix(Sign __instance, string ___m_name, Text ___m_textWidget)
+            static void Prefix(string ___m_name, Text ___m_textWidget, ref string __state)
             {
-                UpdatePicture(__instance, ___m_name, ___m_textWidget);
+                if (___m_name == ValheimPictureFrame.TOKEN_NAME)
+                {
+                    __state = ___m_textWidget.text;
+                }
+            }
+
+            static void Postfix(Sign __instance, string ___m_name, Text ___m_textWidget, string __state)
+            {
+                UpdatePicture(__instance, ___m_name, ___m_textWidget, __state);
             }
         }
 
-        private static void UpdatePicture(Sign __instance, string ___m_name, Text ___m_textWidget)
+        private static void UpdatePicture(Sign __instance, string ___m_name, Text ___m_textWidget, string beforeText)
         {
-            if (___m_name != ValheimPictureFrame.TOKEN_NAME)
+            var names = new string[]
+            {
+                ValheimPictureFrame.TOKEN_NAME, ValheimPictureFrame.TOKEN_NAME_VERTICAL, ValheimPictureFrame.TOKEN_NAME_SQUARE
+            };
+            if ( !names.Any(x => x == ___m_name))
             {
                 return;
             }
 
-            Renderer pictureRenderer = __instance.transform.Find("New/Picture").gameObject.GetComponent<Renderer>();
-            if (pictureRenderer.material.mainTexture != null && pictureRenderer.material.mainTexture.name == ___m_textWidget.text)
+            if (___m_textWidget.text == beforeText)
             {
                 return;
             }
 
-            pictureRenderer.material.mainTexture = ValheimPictureFrame.spriteCache.Load(___m_textWidget.text);
+            var framePivots = new Dictionary<string, Vector3>()
+            {
+                { names[0], new Vector3(0.729f, 0.4473f, 0) },
+                { names[1], new Vector3(0.4473f, 0.729f, 0) },
+                { names[2], new Vector3(0.4473f, 0.4473f, 0) },
+            };
+
+            var text = ___m_textWidget.text.Split(':');
+            GameObject pictureFrame = __instance.gameObject;
+            Transform pivotObject = pictureFrame.transform.Find("Pivot");
+            //Vector3 currentRotation = pictureFrame.transform.rotation.eulerAngles;
+            //pictureFrame.transform.rotation = Quaternion.Euler(0, currentRotation.y, currentRotation.z);
+            pictureFrame.transform.localScale = Vector3.one;
+            pivotObject.transform.localPosition = Vector3.zero;
+
+            if (text.Length == 2)
+            {
+                string[] args = text[1].Split(' ');
+                var options = ParseOptions(args);
+                Vector3 pivotOffset = new Vector3(0, 0, 0);
+
+                if (options.ContainsKey("pivot") || options.ContainsKey("p"))
+                {
+                    string key = options.ContainsKey("pivot") ? "pivot" : "p";
+                    string[] pivots = options[key].Split(',');
+                    foreach (var pivot in pivots)
+                    {
+                        switch (pivot)
+                        {
+                            case "t":
+                            case "top":
+                                pivotOffset += new Vector3(0, -framePivots[___m_name].y, 0);
+                                break;
+                            case "b":
+                            case "bottom":
+                                pivotOffset += new Vector3(0, framePivots[___m_name].y, 0);
+                                break;
+                            case "r":
+                            case "right":
+                                pivotOffset += new Vector3(framePivots[___m_name].x, 0, 0);
+                                break;
+                            case "l":
+                            case "left":
+                                pivotOffset += new Vector3(-framePivots[___m_name].x, 0, 0);
+                                break;
+
+                        }
+                    }
+                    pivotOffset += new Vector3(0, 0, 0.023f);
+                }
+
+                if (options.ContainsKey("scale") || options.ContainsKey("s"))
+                {
+                    string key = options.ContainsKey("scale") ? "scale" : "s";
+                    float scale = Math.Min(float.Parse(options[key]), 10.0f);
+                    pivotObject.transform.localPosition = pivotOffset;
+                    pictureFrame.transform.localScale = Vector3.one * scale;
+                    pivotObject.transform.localPosition -= pivotOffset / scale;
+                }
+
+                //if (options.ContainsKey("tilt") || options.ContainsKey("t"))
+                //{
+                //    string key = options.ContainsKey("tilt") ? "tilt" : "t";
+                //    float tilt = float.Parse(options[key]);
+                //    pivotObject.transform.position += new Vector3(0, framePivots[___m_name].y, 0);
+                //    pictureFrame.transform.rotation = Quaternion.Euler(tilt, currentRotation.y, currentRotation.z);
+                //    pivotObject.transform.position -= new Vector3(0, framePivots[___m_name].y, 0);
+                //}
+
+                //if (options.ContainsKey("orientation") || options.ContainsKey("o"))
+                //{
+                //    string key = options.ContainsKey("orientation") ? "orientation" : "o";
+                //    string orientation = options[key];
+                //    int rotate = 0;
+
+                //    switch (orientation)
+                //    {
+                //        case "vertical":
+                //        case "v":
+                //            rotate = 90;
+                //            break;
+                //        case "horizontal":
+                //        case "h":
+                //            rotate = 0;
+                //            break;
+                //        default:
+                //            break;
+                //    }
+
+                //    pictureFrame.transform.rotation = Quaternion.Euler(currentRotation.x, currentRotation.y, rotate);
+                //}
+
+            }
+
+            Renderer pictureRenderer = __instance.transform.Find("Pivot/New/Picture").gameObject.GetComponent<Renderer>();
+            var textureName = text[0].Trim();
+            pictureRenderer.material.mainTexture = ValheimPictureFrame.spriteCache.Load(textureName);
+        }
+
+        private static Dictionary<string, string> ParseOptions(string[] args)
+        {
+            var options = new Dictionary<string, string>();
+            foreach (var arg in args)
+            {
+                string[] option = arg.Split('=');
+                if (option.Length == 2)
+                {
+                    options.Add(option[0], option[1]);
+                }
+            }
+
+            return options;
         }
     }
 }
--- ValheimPictureFrame/ValheimPictureFrame.cs
+++ ValheimPictureFrame/ValheimPictureFrame.cs
@@ -4,17 +4,20 @@ using Jotunn;
 using Jotunn.Configs;
 using Jotunn.Entities;
 using Jotunn.Managers;
+using System;
 using UnityEngine;
 using ValheimPictureFrame.Utils;
 
 namespace ValheimPictureFrame
 {
-    [BepInPlugin("dfirst.ValheimPictureFrame", "Valheim Picture Frame", "1.0.0")]
+    [BepInPlugin("dfirst.ValheimPictureFrame", "Valheim Picture Frame", "1.1.0")]
     [BepInDependency(Main.ModGuid)]
     public class ValheimPictureFrame : BaseUnityPlugin
     {
         public static readonly TextureCache spriteCache = new TextureCache("ValheimPictureFrame/Assets/Images");
         public static readonly string TOKEN_NAME = "$piece_dfirst_pictureframe";
+        public static readonly string TOKEN_NAME_VERTICAL = "$piece_dfirst_pictureframe_vertical";
+        public static readonly string TOKEN_NAME_SQUARE = "$piece_dfirst_pictureframe_square";
         private static readonly string TOKEN_DESC = "$piece_dfirst_pictureframe_description";
         private readonly Harmony harmony = new Harmony("dfirst.ValheimPictureFrame");
 
@@ -27,35 +30,37 @@ namespace ValheimPictureFrame
         private void AddPiece()
         {
             var assetBundle = AssetBundleHelper.GetAssetBundleFromResources("pictureframe");
-            var asset = assetBundle.LoadAsset<GameObject>($"Assets/Pieces/PictureFrame/PictureFrame.prefab");
-
-            Sign sign = asset.GetComponent<Sign>();
-            sign.m_name = TOKEN_NAME;
-
-            CustomPiece piece = new CustomPiece(asset,
-                new PieceConfig()
-                {
-                    PieceTable = "Hammer",
-                    Description = TOKEN_DESC,
-                    Requirements = new[]
+            string[] prefabNames = new string[] { "PictureFrame", "PictureFrameVertical", "PictureFrameSquare"};
+            PieceConfig config = new PieceConfig()
+            {
+                PieceTable = "Hammer",
+                Description = TOKEN_DESC,
+                Requirements = new[]
+    {
+                    new RequirementConfig()
                     {
-                        new RequirementConfig()
-                        {
-                            Item = "FineWood",
-                            Amount = 6,
-                            Recover = true
-                        },
-                        new RequirementConfig()
-                        {
-                            Item = "BronzeNails",
-                            Amount = 2,
-                            Recover = true
-                        }
+                        Item = "FineWood",
+                        Amount = 6,
+                        Recover = true
+                    },
+                    new RequirementConfig()
+                    {
+                        Item = "BronzeNails",
+                        Amount = 2,
+                        Recover = true
                     }
-                });
+                }
+            };
+            
+            foreach (var prefabName in prefabNames)
+            {
+                var prefab = assetBundle.LoadAsset<GameObject>($"Assets/Pieces/PictureFrame/{prefabName}.prefab");
+                var piece = new CustomPiece(prefab, config);
+                piece.FixReference = true;
+                PieceManager.Instance.AddPiece(piece);
 
-            piece.FixReference = true;
-            PieceManager.Instance.AddPiece(piece);
+            }
+
             assetBundle.Unload(false);
         }
     }
